36ec6436106d0d54613324fbd1254f049463a1ed
YuriMenechelli <shadowarrior19@hotmail.com>
2025-06-06 20:35:09 -0300

    Fix SQL select queries

--- application/models/admin/Users_model.php
+++ application/models/admin/Users_model.php
@@ -4,22 +4,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users_model extends CI_Model{
 
 
-	public function getUsers(){
-		$this->db->select('users.id,
-							CONCAT (users.first_name,
-							" "'.',
-							users.last_name) as nome, 
-							users.email, 
-							users.active,
-							positions.position_name, 
-							departments.department_name,
-							phones.phone,
-							phones.cellphone'
-		);
-		$this->db->from('users');
-		$this->db->join('positions','positions.id=users.id_position','left');
-		$this->db->join('departments','departments.id=positions.id_department','left');
-		$this->db->join('phones','phones.id=users.phone_id','left');
+        public function getUsers(){
+                $this->db->select("users.id,
+                                        CONCAT(users.first_name, ' ', users.last_name) as nome,
+                                        users.email,
+                                        users.active,
+                                        positions.position_name,
+                                        departments.department_name,
+                                        phones.phone,
+                                        phones.cellphone");
+                $this->db->from('users');
+                $this->db->join('positions','positions.id=users.id_position','left');
+                $this->db->join('departments','departments.id=positions.id_department','left');
+                $this->db->join('phones','phones.id=users.phone_id','left');
 		$this->db->order_by('users.id','asc');
 		$this->db->limit(10);
 		$query = $this->db->get();
